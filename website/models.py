42bcf4baba2e2fe41caa547fe5eeb2a619086ba3
Yusuf Yasin <yusufnyasin@gmail.com>
2020-06-18 13:23:32 +0800

    Add Company Database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Company(models.Model):
+	# Info
+	company_name = models.CharField(max_length=50)
+	company_logo = models.FileField()
+	company_email = models.EmailField(max_length=50)
+	company_password = models.CharField(max_length=50)
+	company_number = PhoneNumberField()
+
+	# Addres
+	company_address = models.CharField(max_length=200)
+	company_post_code = models.CharField(max_length=200)
+	company_country = models.CharField(max_length=200)
+	company_state = models.CharField(max_length=200)
