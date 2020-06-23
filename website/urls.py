dab0288d256aeb63d76dd380cefb1c370e20afc2
Yusuf Yasin <yusufnyasin@gmail.com>
2020-06-23 16:59:06 +0800

    Remove register.html and references

--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('home.html', views.home, name="home"),
     path('login.html', views.login, name="login"),
     path('qrform.html', views.qrform, name="qrform"),
-    path('register.html', views.register, name="register"),
     path('dashboard.html', views.dashboard, name="dashboard"),
     path('dashboard_table.html', views.dashboard_table, name="dashboard_table"),
 ]
